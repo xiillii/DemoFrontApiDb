b8ba8227165abed3f213f08b0eaace0c6cb7255a
Jose Alonso <mail@josealonso.dev>
2024-03-07 16:30:55 -0600

    Create domain Product entity

---
+++ src/core/SportsStore.Core.Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿using SportsStore.Core.Domain.Common;
+
+namespace SportsStore.Core.Domain;
+
+public class Product : BaseEntity
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public decimal Price { get; set; }
+}
