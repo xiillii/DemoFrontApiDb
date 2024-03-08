4de55be4c790a2050198ad8ae05f5a531e0e3350
Jose Alonso <mail@josealonso.dev>
2024-03-07 22:09:33 -0600

    Initial database migration
    dotnet ef migrations add InitialCreate -p ..\..\infrastructure\SportsStore.Infrastructure.Persistence\SportsStore.Infrastructure.Persistence.csproj

---
+++ src/infrastructure/SportsStore.Infrastructure.Persistence/Migrations/20240308040808_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SportsStore.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(70)", maxLength: 70, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Category = table.Column<string>(type: "character varying(70)", maxLength: 70, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DateModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "DateCreated", "DateModified", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Watersports", null, null, "A boat for one person", "Kayak", 123.23m },
+                    { 2, "Watersports", null, null, "Protective and fashionable", "Lifejacket", 48m },
+                    { 3, "Soccer", null, null, "Giving your playing field that professional touch", "Corneer flags", 34.23m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
