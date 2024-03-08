8c42305ce276487d15ada7de3403060b16760199
Jose Alonso <mail@josealonso.dev>
2024-03-07 22:23:49 -0600

    API. Product Controller. Get one, create and delete

--- src/api/SportsStore.Api/Controllers/ProductsController.cs
+++ src/api/SportsStore.Api/Controllers/ProductsController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Core.Application.Features.Product.Commands.CreateProduct;
+using SportsStore.Core.Application.Features.Product.Commands.DeleteProduct;
+using SportsStore.Core.Application.Features.Product.Queries.GetProductDetails;
 using SportsStore.Core.Application.Features.Product.Queries.GetProducts;
 
 namespace SportsStore.Api.Controllers
@@ -17,11 +20,46 @@ namespace SportsStore.Api.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<List<ProductDto>>> GetList()
         {
             var products = await _mediator.Send(new GetProductsQuery());
 
             return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<ProductDetailsDto>> GetProductDetails(int id)
+        {
+            var product = await _mediator.Send(new GetProductDetailsQuery(id));
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult> CreateProduct(CreateProductCommand request)
+        {
+            var response = await _mediator.Send(request);
+
+            return CreatedAtAction(nameof(GetProductDetails), new { id = response }, null);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            await _mediator.Send(new DeleteProductCommand(id));
+
+            return Ok();
         }
     }
 }
