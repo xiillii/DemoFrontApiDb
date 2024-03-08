3069000255ef00654e4b31966bcc77aabfc65996
Jose Alonso <mail@josealonso.dev>
2024-03-07 23:46:00 -0600

    Initial MVC Client

---
+++ src/ui/SportsStore.MvcClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SportsStore.MvcClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
